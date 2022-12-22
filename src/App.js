e4ae285e00d2eb06d1f7b1d53aa4a4e9c5015178
unknown <virkanmol074@gmail.com>
2022-12-22 15:22:01 +0530

    react Props

--- src/App.js
+++ src/App.js
@@ -1,35 +1,14 @@
-import { render } from '@testing-library/react';
+// import { render } from '@testing-library/react';s
 import './App.css';
-import elephant from "./images/elephant.jpeg";
 
-const imageData = ()=>{
-  let data = [
-    {
-      id:1,
-      img:elephant
-    },
-    {
-      id:2,
-      img:elephant
-    },
-    {
-      id:3,
-      img:elephant
-    },
-    {
-      id:4,
-      img:elephant
-    }
-  ]
-  return data;
-}
 
-function App() {
+function App({anmol}) {
+  console.log(anmol)
   return (
     <div className='app'>
       <h1>Kalvium Gallary</h1>'
       <div className='gallery'>
-        {imageData().map((elt)=>{
+        {anmol.map((elt)=>{
           return <img src={elt.img} alt="" />
         })}
       </div>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 // import AppClass from "./AppClass";
+import data from './components/DataComponent'
+import AppClass from './AppClass';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />   
+    <AppClass anmol={data} />   
     {/* change the above line - for interchaning between App and AppClass */}
   </React.StrictMode>
 );
